5a57014aab3dc4e9197d548ecf0073d69708ea71
guanfangdong <dong.guanfang.2014@gmail.com>
2021-04-09 11:33:45 +0800

    read_sl class allow read from a train data

---
+++ read_sl.py
@@ -0,0 +1,65 @@
+class ReadSL():
+    """
+    input: file address
+    output: call method get_attrs to get following variables
+            str_var: variables to represent input data
+            str: initial string that used in DSL
+            int_var: variables to represent input int data
+            int: initial int that used in DSL
+            input: input string database
+            output: output string database
+    """
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.str_var = []
+        self.str = []
+        self.int_var = []
+        self.int = []
+        self.input = []
+        self.output = []
+        self.read()
+
+    def parse_line(self, line):
+        if "constraint" in line:
+            indicies = [i for i, x in enumerate(line) if x == '"']
+            input = line[indicies[0]+1:indicies[1]]
+            output = line[indicies[2]+1:indicies[3]]
+            self.input.append(input)
+            self.output.append(output)
+        else:
+            lines = line.split(" ")
+            try:
+                self.int = [int(i) for i in lines]
+            except:
+                if "arg" in lines[0]:
+                    self.str_var = lines
+                else:
+                    print(line)
+                    indicies = [i for i, x in enumerate(line) if x == '"']
+                    print(indicies)
+                    for i in range(0, len(indicies), 2):
+                        self.str.append(line[indicies[i]+1:indicies[i+1]])
+
+    def read(self):
+        f = open(self.file_name, "r")
+        lines = f.readlines()
+        for step, line in enumerate(lines):
+            if "ntString String" in line:
+                str_var = lines[step + 1].strip()
+                str = lines[step + 2].strip()
+            if "ntInt Int" in line:
+                int_var = lines[step + 1].strip()
+                int = lines[step + 2].strip()
+            if "constraint" in line:
+                io_pair = line.strip()
+                self.parse_line(io_pair)
+        for i in [str_var, str, int_var, int]:
+            self.parse_line(i)
+
+    def get_attrs(self):
+        return (self.str_var, self.str, self.int_var, 
+            self.int, self.input, self.output)
+
+if __name__ == "__main__":
+    f = ReadSL("11440431.sl")
+    print(f.get_attrs())
